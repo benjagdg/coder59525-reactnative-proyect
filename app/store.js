1fec65491504d02e49d53739e786034d1b7c516c
Benjamín García del Gaiso <benja.garcia.dg@gmail.com>
2024-11-11 02:38:51 -0300

    add first slice on redux to shop

--- app/store.js
+++ app/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import shopReducer from "../features/shop/shopSlice";
+
 export const store = configureStore({
   reducer: {
+    shopReducer: shopReducer,
 
   },
   middleware: (getDefaultMiddleware) => 
